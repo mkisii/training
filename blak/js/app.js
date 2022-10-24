48d237beae6b85dcd63a9a4acb3b862bbf292c72
obedondimu <ondimuobed@gmail.com>
2022-10-24 09:40:34 +0300

    js-carousel

--- blak/js/app.js
+++ blak/js/app.js
@@ -1,7 +1,9 @@
-// const button = document.querySelectorAll("[data-button]");
-let button = document.querySelector(".next");
-let button1 = document.querySelector(".prev");
+const btns = document.querySelectorAll("[data-button]");
+// let button = document.querySelector(".next");
+// let button1 = document.querySelector(".prev");
 const landing = document.querySelector("#landing-page");
+
+
 
 const image = [
     "url (../assents/img/slider/15.jpg)",
@@ -10,18 +12,43 @@ const image = [
     "url (../assents/img/slider/18.jpg)",
     "url (../assents/img/slider/19.jpg)",
 ];
-console.log(image);
+
+// const noImage = image.length;
+// console.log(noImage);
+
+let current = 0;
+// console.log(btns)
 
 
-button.addEventListener('click',() => {
-    landing.style.backgroundImage ="url(../assents/img/slider/16.jpg)";
-    // console.log('click');
+
+
+
+btns.forEach(btn => {
+
+    btn.addEventListener('click', () => {
+        for (i = 0; i < image.length; i++) {
+
+            landing.style.backgroundImage = i;
+
+            console.log(landing)
+        }
+        // landing.style.backgroundImage = "url(../assents/img/slider/16.jpg)";
+        // if (btn === "next") {
+
+        //     landing.style.backgroundImage = 
+        // }
+
+        // console.log('click');
+    });
+    // button1.addEventListener('click', () => {
+    //     landing.style.backgroundImage = "url(../assents/img/slider/19.jpg)";
+    //     // console.log('click');
+    // });
+    // button.forEach(button => {
+
+    // })
+
+
 });
-button1.addEventListener('click',() => {
-    landing.style.backgroundImage ="url(../assents/img/slider/19.jpg)";
-    // console.log('click');
-});
-// button.forEach(button => {
 
-// })
 
