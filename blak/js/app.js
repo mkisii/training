ff6af5d70a3236d0f734d43e32ce9f0f9419c582
obedondimu <ondimuobed@gmail.com>
2022-10-28 11:39:07 +0300

    service-page

--- blak/js/app.js
+++ blak/js/app.js
@@ -2,52 +2,37 @@ const btns = document.querySelectorAll("[data-button]");
 // let button = document.querySelector(".next");
 // let button1 = document.querySelector(".prev");
 const landing = document.querySelector("#landing-page");
+console.log(btns)
 
 
 
 const image = [
-    "url (../assents/img/slider/15.jpg)",
-    "url (../assents/img/slider/16.jpg)",
-    "url (../assents/img/slider/17.jpg)",
-    "url (../assents/img/slider/18.jpg)",
-    "url (../assents/img/slider/19.jpg)",
+    "url(../assents/img/slider/15.jpg)",
+    "url(../assents/img/slider/16.jpg)",
+    "url(../assents/img/slider/17.jpg)",
+    "url(../assents/img/slider/18.jpg)",
+    "url(../assents/img/slider/19.jpg)",
 ];
 
-// const noImage = image.length;
-// console.log(noImage);
+landing.style.backgroundImage = image[0];
+let counter=0;
 
-let current = 0;
-// console.log(btns)
-
-
-
-
-
-btns.forEach(btn => {
-
-    btn.addEventListener('click', () => {
-        for (i = 0; i < image.length; i++) {
-
-            landing.style.backgroundImage = i;
-
-            console.log(landing)
+btns.forEach( (btn)=>{
+    btn.addEventListener('click',()=>{
+        landing.style.backgroundImage= image[counter];
+        if(btn.id==='prev'){
+            counter -=1;
+            if(counter < 0){
+                counter = image.length-1;
+            }
+        }else if(btn.id==='next'){
+            counter+=1;
+            if(counter>image.length-1){
+                counter = 0;
+            }
+            
         }
-        // landing.style.backgroundImage = "url(../assents/img/slider/16.jpg)";
-        // if (btn === "next") {
-
-        //     landing.style.backgroundImage = 
-        // }
-
-        // console.log('click');
-    });
-    // button1.addEventListener('click', () => {
-    //     landing.style.backgroundImage = "url(../assents/img/slider/19.jpg)";
-    //     // console.log('click');
-    // });
-    // button.forEach(button => {
-
-    // })
-
+    })
 
 });
 
